cfe03dbd104ef5a7383fe7689f2e44a397200035
loottura_ina <loottura@gmail.com>
2024-08-30 16:59:16 +0900

    first-build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'git@github.com:inamathi/sugadaira-davos-teambuilding.git',
   plugins: [react()],
 })
